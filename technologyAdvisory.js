2cdeaf63fe27b50133d1e2029de957b80fe08964
Matteo <99515747+Matstraz@users.noreply.github.com>
2022-05-09 20:10:19 +0200

    feat: add modal js

--- technologyAdvisory.js
+++ technologyAdvisory.js
@@ -1,19 +1,42 @@
-let off1 = document.querySelector('.callback')
+let offButton = document.querySelector('.callback')
 let input1 = document.querySelector('.canvasName').value
 let input2 = document.querySelector('.canvasTel').value
 
-off1.onclick = function (event){
+offButton.onclick = function (event){
     event.preventDefault()
     console.log(`success: ${input1} ${input2}`)
 }
 
-
-/* //ALTERNATIVA PER CREARE UN ARRAY (rinominare le classi negli input)
-let off1 = document.querySelector('.callback')
+/* //ALTERNATIVA PER CREARE UN ARRAY
+let offButton = document.querySelector('.callback')
 let input1 = document.querySelectorAll('.offCavansLarge')
 
-off1.onclick = function (event){
+offButton.onclick = function (event){
     event.preventDefault()
-    let obj = []
-    input1.forEach((element, i) => obj.push((input1[i].id) = input1[i].value));
-    console.log(`success: ${obj}`) */
+    let arr = []
+    input1.forEach((element, i) => arr.push([input1[i].id, input1[i].value]));
+    console.log(arr)
+} */
+
+/* //ALTERNATIVA PER CREARE UN OGGETTO
+let offButton = document.querySelector('.callback')
+let input1 = document.querySelectorAll('.offCavansLarge')
+
+offButton.onclick = function (event){
+    event.preventDefault()
+    let obj = {}
+    input1.forEach((element, i) => obj[input1[i].id] = input1[i].value);
+    console.log(obj)
+} */
+
+let modButtom = document.querySelector('.modalButton')
+let selects = document.querySelectorAll('.modalSelect')
+
+
+modButtom.onclick = function (event){
+    event.preventDefault()
+    let obj = {}
+    selects.forEach((element, i) => obj[selects[i].id] = selects[i].value);
+    console.log(obj)
+}
+
