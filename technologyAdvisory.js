aea88e9a5e5177107ba5cd267a61cb4f61a2f8bd
Matteo <99515747+Matstraz@users.noreply.github.com>
2022-05-09 18:17:05 +0200

    feat: fix off canvas large

--- technologyAdvisory.js
+++ technologyAdvisory.js
@@ -4,7 +4,16 @@ let input2 = document.querySelector('.canvasTel').value
 
 off1.onclick = function (event){
     event.preventDefault()
-    console.log(`success:Jacopo, 33532123 ${input1} ${input2}`)
+    console.log(`success: ${input1} ${input2}`)
 }
 
-let modalForm = document.querySelector('.modal-body').firstElementChild
+
+/* //ALTERNATIVA PER CREARE UN ARRAY (rinominare le classi negli input)
+let off1 = document.querySelector('.callback')
+let input1 = document.querySelectorAll('.offCavansLarge')
+
+off1.onclick = function (event){
+    event.preventDefault()
+    let obj = []
+    input1.forEach((element, i) => obj.push((input1[i].id) = input1[i].value));
+    console.log(`success: ${obj}`) */
